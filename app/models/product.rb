e14dfd3686921f64cdfd6cc83163d79a58321c9b
= <=>
2013-09-24 16:09:50 -0400

    made the name field for products accessible for mass assignment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :brand, :description, :ingredient_ids
+  attr_accessible :brand, :description, :name, :ingredient_ids
   has_and_belongs_to_many :ingredients, :join_table => :ingredients_products
   accepts_nested_attributes_for :ingredients, :reject_if => lambda { |a| a[:name].blank? }
 end
